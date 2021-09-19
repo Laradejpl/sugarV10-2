b7daad31c3893fe57313162f2ede92f51046db2e
philDu45 <pablolarade@gmail.com>
2021-09-19 16:48:39 +0200

    mise en place du chat en ajax

---
+++ messages.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once __DIR__ . '/config/bootstrap.php';
+//var_dump(getMembre()['pseudo']);
+         switch ($_REQUEST['action']) {
+             case 'sendMessage':
+                 $req = $pdo->prepare("INSERT INTO messages ( user,message) VALUES(?,?)");
+                  $run = $req->execute([getMembre()['pseudo'],$_REQUEST['message']]);
+                 
+                  if($run){
+
+                    echo 1;
+                    exit;
+
+                  }
+                
+                 
+                 break;
+
+
+                 case 'getMessages' :
+                    
+                    $req = $pdo->prepare("SELECT * FROM messages ");
+                    $run = $req->execute();
+                    $chat = "";
+                    $rs = $req->fetchAll(PDO::FETCH_OBJ);
+                      foreach ($rs as $message) {
+                         
+
+                          $chat .= '<div class="single-message">
+                            <strong>'.$message->user .'</strong>' . $message->message.'
+                          
+                        <span style="float:right;"> '.date( 'h:m:a',strtotime ($message->date)).'</span>
+                                   </div>';
+
+                                       
+                      }
+                   
+                       echo $chat;
+             
+             default:
+                 # code...
+                 break;
+         }
+
+?>
